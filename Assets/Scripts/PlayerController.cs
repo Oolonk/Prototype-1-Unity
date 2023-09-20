c44a84f893688f98b159d3c5401e8ad69eb7ebee
Oolonk <dortimus51@gmail.com>
2023-09-20 23:17:34 +0200

    Lesson 1.2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+
+    //Update is called every fixed value
+    private void FixedUpdate()
+    {
+
+    }
+}
