6b5be28105d4db56f29300ecb59ea7c61cd7c035
Oolonk <dortimus51@gmail.com>
2023-10-08 20:30:18 +0200

    add idea folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * (Time.deltaTime * 20));
     }
 
     //Update is called every fixed value
