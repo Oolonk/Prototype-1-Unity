45d4ee7db905c45c0d9e8d33882c95845eb641a5
Oolonk <dortimus51@gmail.com>
2023-11-10 18:56:04 +0100

    Lesson 1.3 and 1.4

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class FollowPlayer : MonoBehaviour
+{
+    //Aufrufen eines GameObjektes
+    public GameObject player;
+    //Der Offset von der Kamera
+    private Vector3 cameraOffset = new Vector3(0, 5, -7);
+    // Aufgerufen während der Initialisierung
+    void Start()
+    {
+    }
+
+    // Spätere Hierarchie wann es ausgeführt werden soll von Update()
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,31 @@
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Horizontale Geschwindigkeit vom Spieler
+    private float horizontalInput;
+    // Beschleunigung vom Spieler
+    private float forwardInput;
+    // Geschwindigkeit des Autos
+    private float speed = 20.0f;
+    //Drehgeschwindigkeit des Autos
+    private float turnSpeed = 40.0f;
+    // Aufgerufen während der Initialisierung
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    // Wird jeden Frame abgerufen
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * 20));
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        transform.Rotate(Vector3.up * (Time.deltaTime * turnSpeed * horizontalInput));
     }
 
-    //Update is called every fixed value
+    //Wird immer eine gefixte Anzahl an Zeit abgerufen
     private void FixedUpdate()
     {
 
